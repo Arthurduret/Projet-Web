e5d8f6f5f441b9e78575d4a246e7e29aaf63bbd0
DURET Arthur <arthur.duret@arlovic.fr>
2026-02-06 16:33:19 +0100

    Fix : Changement de tout les fichier .html en .php avec intégration des header et footer en php.

--- header.php
+++ header.php
@@ -1,17 +1,17 @@
 <header>
-    <a href="index.php" class="logo">
+    <a href="/index.php" class="logo">
         <img src="/images/LogoJobeo.png" alt="Logo" width="90" height="50">
     </a>
 
     <nav id="nav-menu">
         <a href="#entreprises">Nos Entreprises</a>
-        <a href="a_propos.php">A propos de nous</a>
-        <a href="offres_emplois.php">Offres d'emplois</a>
-        <a href="favoris.php">Favoris</a>
+        <a href="/a_propos.php">A propos de nous</a>
+        <a href="/offres_emplois.php">Offres d'emplois</a>
+        <a href="/favoris.php">Favoris</a>
     </nav>
 
     <div class="header-right">
-        <a href="identifier.php" class="btn-compte">
+        <a href="/identifier.php" class="btn-compte">
             <img src="/images/logo_profil.png" alt="logo_profil" width="40" height="40">
             <span>Mon compte</span>
         </a>
