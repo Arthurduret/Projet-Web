18a1f51fda1f2801e8d4726c5c467aac2b1014bb
DURET Arthur <arthur.duret@arlovic.fr>
2026-02-06 15:30:01 +0100

    Ajout du header en php responsive

--- header.php
+++ header.php
@@ -1,16 +1,30 @@
-    <header>
-        <a href="index.php" class="logo">
-            <img src="images/LogoJobeo.png" alt="Logo" width="90" height="50">
-        </a>
-        <nav>
-            <a href="#entreprises" class="hover:underline focus:outline-none focus:ring-2 focus:ring-orange-500">Nos Entreprises</a>
-            <a href="a_propos.php">A propos de nous</a>
-            <a href="offres_emplois.php">Offres d'emplois</a>
-            <a href="favoris.php">Favoris</a>
+<header>
+    <a href="index.html" class="logo">
+        <img src="images/LogoJobeo.png" alt="Logo" width="90" height="50">
+    </a>
 
-            <a href="identifier.html" class="btn-compte">
-                <img src="images/logo_profil.png" alt="logo_profil" width="50" height="50">
-                    Mon compte 
-            </a>
-        </nav>
-    </header>
+    <nav id="nav-menu">
+        <a href="#entreprises">Nos Entreprises</a>
+        <a href="a_propos.html">A propos de nous</a>
+        <a href="offres_emplois.html">Offres d'emplois</a>
+        <a href="favoris.html">Favoris</a>
+    </nav>
+
+    <div class="header-right">
+        <a href="identifier.html" class="btn-compte">
+            <img src="images/logo_profil.png" alt="logo_profil" width="40" height="40">
+            <span>Mon compte</span>
+        </a>
+        
+        <div class="menu-tel" onclick="toggleMenu()">☰</div>
+    </div>
+
+    <script>
+        function toggleMenu() {
+            const nav = document.getElementById('nav-menu');
+            // Ajoute ou enlève la classe "active" à chaque clic
+            nav.classList.toggle('active');
+        }
+    </script>
+
+</header>
