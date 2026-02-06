faa7a4e8659d54938e3bf2ab31322c1a0b618f58
DURET Arthur <arthur.duret@arlovic.fr>
2026-02-06 16:23:28 +0100

    Suppression du deuxieme Header dans index

--- index.php
+++ index.php
@@ -34,15 +34,6 @@
 
 
     </main>
-<<<<<<< HEAD
-    <footer>
-        <a href="/pages_footeur/mentions_legales.html">Mentions Légales</a>
-        <p>✦</p>
-        <a href="/pages_footeur/cgu.html">CGU</a>
-        <p>✦</p>
-        <a href="/pages_footeur/cookies.html">Gestion des cookies</a>
-    </footer>
-=======
     <?php include 'footer.php'; ?>
 >>>>>>> arthur
 </body>
