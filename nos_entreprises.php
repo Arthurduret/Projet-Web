a992a7fb2bb4345f8eedbf5fc53a05b585f6d432
DURET Arthur <arthur.duret@arlovic.fr>
2026-02-10 15:53:52 +0100

    Implementation de nos_entreprises.php

--- nos_entreprises.php
+++ nos_entreprises.php
@@ -9,27 +9,48 @@
 </head>
 
 <body>
-    <?php include '/header.php'; ?>
-
-        <main>
-            <div class="wrapper">
-            <section>
-                <article>
-                    <p>teste</p>
-                </article>
-                <article>
-                    <p>teste2</p>
-                </article>
-                <article>
-                    <p>teste3</p>
-                </article>
-            </section>
+    <?php include 'header.php'; ?>
+    <main>
+        <p>Toutes les entreprises qui recrutent en France</p>
+        <h1>Explorez les entreprises qui recrutent</h1>
+        <div>
+            <div>
+                <div><img src="" alt="test1">
+                </div>
+                <div><img src="" alt="test2">
+                </div>
             </div>
+            <div>
+                <div>
+                    <div>
+                        <a href="/entreprises/leroy_merlin.php">
+                            <h3>Leroy Merlin</h3>
+                        </a>
+                    </div>
+                    <div>
+                        <span>Bricolage</span>
+                        <span>> 10 000 salariés</span>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        Decouvrir
+                    </div>
+                    <div>
+                        <span>389 jobs</span>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
-        </main>
-    
-    <?php include '/footer.php'; ?>
+
+
+
+
+
+    </main>
+    <?php include 'footer.php'; ?>
     
 </body>
 </html>
