aa6d5bf96ff73eb47b26a5864db89c5fedcb44e8
Matis Lavignac <matis.lavignac@viacesi.fr>
2026-02-10 16:44:40 +0100

    pieds de page et css propres

--- pages_footeur/cookies.php
+++ pages_footeur/cookies.php
@@ -3,13 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../header_footer.css">
+    <link rel="stylesheet" href="/style_global.css">
+    <link rel="stylesheet" href="/header_footer.css">
+    <link rel="stylesheet" href="/pages_footeur/legal.css">
+
     <title>Cookies</title>
 </head>
 <body>
     <?php include '../header.php'; ?> 
     <main>
-        <div class="pages_footeur">
+        <div class="pages_footer">
             <h1>Politique de Cookies & Confidentialité</h1>
             
             <h2>Collecte des données</h2>
--- pages_footeur/mentions_legales.php
+++ pages_footeur/mentions_legales.php
@@ -3,45 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../header_footer.css">
+    <link rel="stylesheet" href="/style_global.css">
+    <link rel="stylesheet" href="/header_footer.css">
+    <link rel="stylesheet" href="/pages_footeur/legal.css">
+
     <title>Mentions Légales</title>
 </head>
 <body>
     <?php include '../header.php'; ?> 
     <main>
-        <div class="pages_footeur">
+        <div class="pages_footer">
             <h1> Mentions Légales</h1>
-
-            <h2>
-                Éditeur du site :
-            </h2>
-            <p>
-                Jobeo 
-            </p>
-            <h2>
-                Statut : 
-            </h2>
-            <p>
-                Particuliers 
-            </p>
-            <h2>
-                Siège social :
-            </h2>
-            <p>
-                CESI
-            </p>
-            <h2>
-                Directeur de la publication : 
-            </h2>  
-            <p>
-                Lavignac, Duret, Ceccaldi, Couliard
-            </p>
-            <h2>
-                Hébergement :
-            </h2>
-            <p>
-                Le site est hébergé par OVH.
-            </p>
+            <div class="mentions">
+                <h2>
+                    Éditeur du site :
+                </h2>
+                <p>
+                    Jobeo 
+                </p>
+            </div>
+            <div class="mentions">
+                <h2>
+                    Statut : 
+                </h2>
+                <p>
+                    Particuliers 
+                </p>
+                </div>
+                <div class="mentions">
+                <h2>
+                    Siège social :
+                </h2>
+                <p>
+                    CESI
+                </p>
+                </div>
+                <div class="mentions">
+                <h2>
+                    Directeur de la publication : 
+                </h2>  
+                <p>
+                    Lavignac, Duret, Ceccaldi, Couliard
+                </p>
+                </div>
+                <div class="mentions">
+                <h2>
+                    Hébergement :
+                </h2>
+                <p>
+                    Le site est hébergé par OVH.
+                </p>
+                </div>
         </div>
     </main>    
     <?php include '../footer.php'; ?> 
