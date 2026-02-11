9859470439ae23fa815a9e39dd3c438a9ed5b4fe
Matis Lavignac <matis.lavignac@viacesi.fr>
2026-02-11 12:10:42 +0100

    pages à propos css liens

--- a_propos.php
+++ a_propos.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_global.css">
+    <link rel="stylesheet" href="/pages_footer/legal.css">
     <link rel="stylesheet" href="header_footer.css">
     <title>à-propos-de-nous</title>
 </head>
