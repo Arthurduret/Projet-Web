a37ac888869864ff6df9fdc56192e02705641098
DURET Arthur <arthur.duret@arlovic.fr>
2026-03-09 11:23:35 +0100

    Ajout et implementation du fichier entreprises_modele.php

---
+++ modeles/entreprises_modele.php
@@ -0,0 +1,21 @@
+<?php 
+
+class EntrepriseModele {
+
+    // On stocke la connexion PDO dans la classe
+    private $pdo; 
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+
+    public function getEntreprises() {
+        // $query contient un résultat "brut", PHP ne sait pas encore quoi en faire
+        $query = $this->pdo->query("SELECT * FROM entreprises");
+        // fetchAll() transforme ce résultat en tableau PHP utilisable
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+?>
