febb55b3470f43a8cddefaa9ed5c297e05de7337
Matis Lavignac <matis.lavignac@viacesi.fr>
2026-03-10 16:20:53 +0100

    ajout des names dans le formulaire de 'inscription entreprise

--- vues/inscription_entreprise_vue.php
+++ vues/inscription_entreprise_vue.php
@@ -28,53 +28,50 @@
                 <a class="switch_btn active">Entreprise</a>
             </div>
 
-            <form class="form-entreprise">
+            <form class="form-entreprise" action="inscription_traitement.php" method="POST" enctype="multipart/form-data">
+    
                 <div class="input-group">
                     <label>Email</label>
-                    <input type="email" placeholder="Exemple : prenom.nom@gmail.com" required>
+                    <input type="email" name="email" placeholder="Exemple : prenom.nom@gmail.com" required>
                 </div>
 
                 <div class="input-group">
-                    <div class="input-group">
-                        <label>Nom</label>
-                        <input type="text" placeholder="Nom d'entreprises" required>
-                    </div>
+                    <label>Nom</label>
+                    <input type="text" name="nom_entreprise" placeholder="Nom d'entreprises" required>
                 </div>
-
-
-
 
                 <div class="input-group">
                     <label>Taille d'entreprises</label>
-                    <select required>
+                    <select name="taille_entreprise" required>
                         <option value="">Combien d'employés avez-vous ?</option>
-                        <option>Micro-entreprise (moins de 10 pers)</option>
-                        <option>Petite entreprise (10-50 pers) </option>
-                        <option>Moyenne entreprise (50-250 pers)</option>
-                        <option>Grande entreprise (plus de 250 pers)</option>
+                        <option value="micro">Micro-entreprise (moins de 10 pers)</option>
+                        <option value="petite">Petite entreprise (10-50 pers)</option>
+                        <option value="moyenne">Moyenne entreprise (50-250 pers)</option>
+                        <option value="grande">Grande entreprise (plus de 250 pers)</option>
                     </select>
                 </div>
 
                 <div class="input-group">
-                <label>Domaine d'activité</label>
+                    <label>Domaine d'activité</label>
                     <div class="checkbox-options">
-                        <label><input type="checkbox" name="domaine" value="Informatique"> Informatique</label>
-                        <label><input type="checkbox" name="domaine" value="Marketing"> Marketing</label>
-                        <label><input type="checkbox" name="domaine" value="Finance"> Finance</label>
-                        <label><input type="checkbox" name="domaine" value="Design"> Design</label>
-                        <label><input type="checkbox" name="domaine" value="Communication"> Communication</label>
+                        <label><input type="checkbox" name="domaine[]" value="Informatique"> Informatique</label>
+                        <label><input type="checkbox" name="domaine[]" value="Marketing"> Marketing</label>
+                        <label><input type="checkbox" name="domaine[]" value="Finance"> Finance</label>
+                        <label><input type="checkbox" name="domaine[]" value="Design"> Design</label>
+                        <label><input type="checkbox" name="domaine[]" value="Communication"> Communication</label>
                     </div>
                 </div>
 
                 <div class="input-group">
                     <label>Logo entreprise</label>
-                    <input type="file">
+                    <input type="file" name="logo" accept="image/*">
                 </div>
 
                 <div class="input-group input-group--commentaire">
                     <label for="commentaire">Commentaire supplémentaire</label>
                     <textarea
                         id="commentaire"
+                        name="description"
                         placeholder="Faîtes une description de votre entreprise"
                         rows="5"
                         required
@@ -83,27 +80,28 @@
 
                 <div class="input-group">
                     <label>Mot de passe</label>
-                    <input type="password" placeholder="Votre mot de passe" required>
+                    <input type="password" name="mdp" placeholder="Votre mot de passe" required>
                 </div>
 
                 <div class="input-group">
-                    <label>Mot de passe</label>
-                    <input type="password" placeholder="Confirmer Votre mot de passe" required>
+                    <label>Confirmer Mot de passe</label>
+                    <input type="password" name="mdp_confirm" placeholder="Confirmer Votre mot de passe" required>
                 </div>
                 
                 <div class="checkbox-group">
-                    <input type="checkbox" required>
+                    <input type="checkbox" name="cgu" required>
                     <a href="pages_footeur/cgu.html" >J'accepte les conditions générales d'utilisation</a>
                 </div>
 
                 <div class="checkbox-group">
-                    <input type="checkbox" required>
+                    <input type="checkbox" name="confidentialite" required>
                     <a href="pages_footeur/mentions_legales.html" >J’accepte la politique de confidentialité</a>
                 </div>
                 <button type="submit">S'inscrire</button>
             </form>
         </div>
     </main>
+
 
     <?php include __DIR__ . '/partials/footer.php'; ?>
 
