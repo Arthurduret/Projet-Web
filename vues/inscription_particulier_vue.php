5330bfd9c23a326ae3368ceb46de96e33c12cb39
Baptiste <baptiste.ceccaldi4@gmail.com>
2026-03-13 09:30:37 +0100

    ajout de l'inscription basé sur le MCD

--- vues/inscription_particulier_vue.php
+++ vues/inscription_particulier_vue.php
@@ -10,7 +10,7 @@
 
     <link rel="stylesheet" href="/public/css/style_global.css">
     <link rel="stylesheet" href="/public/css/header_footer.css">
-    <link rel="stylesheet" href="/public/css/inscription.css">
+    <link rel="stylesheet" href="/public/css/Style_connexion.css">
 
 </head>
 
@@ -23,20 +23,9 @@
 
             <a href="#" class="back-link">← Retour</a>
 
-            <h1>S'inscrire</h1>
-            
-            <div class="switch_buttons">
-                <a class="switch_btn active" disabled>Particulier</a>
-                <a href="inscription_entreprise.php" class="switch_btn">Entreprise</a>
-            </div>
+            <h1>Inscription</h1>
 
             <form>
-                
-                <div class="input-group">
-                    <label>Email</label>
-                    <input type="email" placeholder="Exemple : prenom.nom@gmail.com" required>
-                </div>
-
                 <div class="double-input">
                     <div class="input-group">
                         <label>Nom</label>
@@ -50,47 +39,19 @@
                 </div>
 
                 <div class="input-group">
-                    <label>Type de contrat</label>
-                    <select required>
-                        <option value="">Que recherchez-vous ?</option>
-                        <option>CDI</option>
-                        <option>CDD</option>
-                        <option>Stage</option>
-                        <option>Alternance</option>
-                        <option>Freelance</option>
-                    </select>
+                    <label>Email</label>
+                    <input type="email" placeholder="Exemple : prenom.nom@gmail.com" required>
                 </div>
 
-                <div class="input-group">
-                    <label>Domaine d'activité</label>
-                    <select required>
-                        <option value="">Où recherchez-vous ?</option>
-                        <option>Informatique</option>
-                        <option>Marketing</option>
-                        <option>Finance</option>
-                        <option>Design</option>
-                    </select>
-                </div>
+                <div class="input-group radio-container">
+                    <label class="label-radio-titre">Je suis</label>
+                    <div class="radio-group">
+                        <input type="radio" name="role" value="Étudiant" id="Étudiant" required>
+                            <label for="Étudiant">Étudiant</label>
 
-                <div class="input-group">
-                    <label>Expérience</label>
-                    <select>
-                        <option>Année d'expérience</option>
-                        <option>0 - 1 an</option>
-                        <option>1 - 3 ans</option>
-                        <option>3 - 5 ans</option>
-                        <option>5+ ans</option>
-                    </select>
-                </div>
-
-                <div class="input-group">
-                    <label>CV</label>
-                    <input type="file">
-                </div>
-
-                <div class="input-group">
-                    <label>Ressources additionnelles</label>
-                    <input type="file">
+                        <input type="radio" name="role" value="Pilote" id="Pilote">
+                            <label for="Pilote">Pilote</label>
+                    </div>
                 </div>
 
                 <div class="input-group">
@@ -102,6 +63,9 @@
                     <label>Mot de passe</label>
                     <input type="password" placeholder="Confirmer Votre mot de passe" required>
                 </div>
+
+
+
 
                 <div class="checkbox-group">
                     <input type="checkbox" required>
